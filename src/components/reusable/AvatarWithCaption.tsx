50e7c9bcf45aca8d47f6ff41337f763e780a8a69
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-04-21 13:02:09 -0300

    Language Skills Box

--- src/components/reusable/AvatarWithCaption.tsx
+++ src/components/reusable/AvatarWithCaption.tsx
@@ -1,13 +1,13 @@
-import Avatar from "@material-ui/core/Avatar";
-import Box from "@material-ui/core/Box";
+import Avatar from '@material-ui/core/Avatar'
+import Box from '@material-ui/core/Box'
 
-const AvatarWithCaption = ({ avatarAddress, caption }) => {
+const AvatarWithCaption = ({ avatarAddress, caption, alt }) => {
   return (
     <Box>
-      <Avatar alt="Remy Sharp" src={avatarAddress} />
+      <Avatar alt={alt} src={avatarAddress} />
       {caption}
     </Box>
-  );
-};
+  )
+}
 
-export default AvatarWithCaption;
+export default AvatarWithCaption
--- src/components/specific/IndexPage/LateralCard/LanguageSection.tsx
+++ src/components/specific/IndexPage/LateralCard/LanguageSection.tsx
@@ -1,23 +1,37 @@
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
-import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as fullStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
 
-import AvatarWithCaption from "../../../reusable/AvatarWithCaption";
+import AvatarWithCaption from '../../../reusable/AvatarWithCaption'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#92A8A8',
+  },
+}))
 
 const LanguageSection = () => {
+  const classes = useStyles()
+
   return (
-    <Box my={1}>
-      <Grid
-        container
-        direction="row"
-        justify="space-around"
-        alignItems="center"
+    <Box py={2} className={classes.root} boxShadow={3}>
+      <Typography variant="h5" align="center">
+        Language Skills
+      </Typography>
+
+      <Box
+        mt={2}
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-around"
       >
         <AvatarWithCaption
           avatarAddress="/images/languages/uk_flag.png"
+          alt="English Skill Medium"
           caption={
             <>
               <FontAwesomeIcon icon={fullStar} />
@@ -29,6 +43,7 @@ const LanguageSection = () => {
 
         <AvatarWithCaption
           avatarAddress="/images/languages/france_flag.png"
+          alt="French Skill Basic"
           caption={
             <>
               <FontAwesomeIcon icon={fullStar} />
@@ -40,6 +55,7 @@ const LanguageSection = () => {
 
         <AvatarWithCaption
           avatarAddress="/images/languages/brazil_flag.png"
+          alt="Portuguese Skill Fluent"
           caption={
             <>
               <FontAwesomeIcon icon={fullStar} />
@@ -48,9 +64,9 @@ const LanguageSection = () => {
             </>
           }
         />
-      </Grid>
+      </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default LanguageSection;
+export default LanguageSection
