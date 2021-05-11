ad529a3f960ae17914ab5e2ad7dfffdca9940efd
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-05-11 12:59:34 -0300

    Project card formated

--- src/components/specific/Portfolio/Project.tsx
+++ src/components/specific/Portfolio/Project.tsx
@@ -1,24 +1,51 @@
 import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Box from '@material-ui/core/Box'
+import CardHeader from '@material-ui/core/CardHeader'
 
 const useStyles = makeStyles((theme) => ({
   rootMobile: {
     display: 'flex',
     flexDirection: 'column',
+    marginTop: theme.spacing(2),
   },
 
-  rootDesktop: {},
+  rootDesktop: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateAreas: `    
+    "media header"
+    "media content"
+    "media actions"
+    `,
+    padding: theme.spacing(1),
+    alignItems: 'center',
+    marginTop: theme.spacing(2),
+  },
 
-  thumbnail: {
-    width: '560px',
-    height: '315px',
+  headerDesktop: {
+    gridArea: 'header',
+  },
+
+  mediaDesktop: {
+    minWidth: '560px',
+    minHeight: '315px',
+    gridArea: 'media',
+  },
+
+  contentDesktop: {
+    gridArea: 'content',
+  },
+
+  actionsDesktop: {
+    gridArea: 'actions',
+    display: 'flex',
+    justifyContent: 'space-around',
   },
 }))
 
@@ -26,28 +53,55 @@ const Project = ({ desktopLayout }) => {
   const classes = useStyles()
 
   return (
-    <Card elevation={1}>
-      <CardActionArea>
-        <Typography
-          variant="h4"
-          align="center"
-          component="h2"
-          className={desktopLayout ? classes.rootDesktop : classes.rootMobile}
-        >
-          Dummy Coordinate
-        </Typography>
-        <CardMedia
-          component="iframe"
-          image="https://www.youtube.com/embed/Ke90Tje7VS0"
-          className={classes.thumbnail}
-          title="Contemplative Reptile"
-        />
+    <Card
+      elevation={1}
+      variant="outlined"
+      className={`${desktopLayout ? classes.rootDesktop : classes.rootMobile}`}
+    >
+      <CardHeader
+        title="Dummy Coordinate"
+        align="center"
+        className={classes.headerDesktop}
+      />
 
+      <CardMedia
+        component="iframe"
+        image="https://www.youtube.com/embed/Ke90Tje7VS0"
+        className={classes.mediaDesktop}
+        title="Contemplative Reptile"
+      />
+
+      <CardContent className={classes.contentDesktop}>
         <Box>
-          <Typography display="inline">Description: </Typography>
+          <Typography variant="h6" display="inline">
+            Type:{' '}
+          </Typography>
           <Typography display="inline">Lorem </Typography>
         </Box>
-      </CardActionArea>
+
+        <Box>
+          <Typography variant="h6" display="inline">
+            Description:{' '}
+          </Typography>
+          <Typography display="inline">Lorem </Typography>
+        </Box>
+
+        <Box>
+          <Typography variant="h6" display="inline">
+            Tech Stack:{' '}
+          </Typography>
+          <Typography display="inline">Lorem </Typography>
+        </Box>
+      </CardContent>
+
+      <CardActions className={classes.actionsDesktop}>
+        <Button variant="contained" color="primary" size="small">
+          See Code
+        </Button>
+        <Button variant="contained" color="primary" size="small">
+          Visit Page
+        </Button>
+      </CardActions>
     </Card>
   )
 }
