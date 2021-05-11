b6eefbb04803c602b668eff4a972c917fa7232b3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-05-11 11:03:21 -0300

    Project component started

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -2,13 +2,13 @@ import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import Project from '../src/components/specific/Portfolio/Project'
 
-const Portfolio = () => {
+const Portfolio = ({ desktopLayout }) => {
   return (
     <Container>
       <Typography variant="h1" align="center">
         Projects Page
       </Typography>
-      <Project />
+      <Project desktopLayout={desktopLayout} />
     </Container>
   )
 }
--- src/components/specific/Portfolio/Project.tsx
+++ src/components/specific/Portfolio/Project.tsx
@@ -1,13 +1,54 @@
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Box from '@material-ui/core/Box'
 
-const Project = () => {
+const useStyles = makeStyles((theme) => ({
+  rootMobile: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  rootDesktop: {},
+
+  thumbnail: {
+    width: '560px',
+    height: '315px',
+  },
+}))
+
+const Project = ({ desktopLayout }) => {
+  const classes = useStyles()
+
   return (
-    <Paper elevation={3}>
-      <Typography variant="h4" align="center" component="h2">
-        Dummy Coordinate
-      </Typography>
-    </Paper>
+    <Card elevation={1}>
+      <CardActionArea>
+        <Typography
+          variant="h4"
+          align="center"
+          component="h2"
+          className={desktopLayout ? classes.rootDesktop : classes.rootMobile}
+        >
+          Dummy Coordinate
+        </Typography>
+        <CardMedia
+          component="iframe"
+          image="https://www.youtube.com/embed/Ke90Tje7VS0"
+          className={classes.thumbnail}
+          title="Contemplative Reptile"
+        />
+
+        <Box>
+          <Typography display="inline">Description: </Typography>
+          <Typography display="inline">Lorem </Typography>
+        </Box>
+      </CardActionArea>
+    </Card>
   )
 }
 
